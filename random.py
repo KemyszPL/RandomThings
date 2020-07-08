9c4cb891b07ac6101f7eac57e60c7b45cef0c28a
KemyszPL <40953832+KemyszPL@users.noreply.github.com>
2020-07-08 21:30:23 +0200

    added insults and made insult option functional

--- random.py
+++ random.py
@@ -8,12 +8,13 @@ print("Currently avilable Random Stuff™️:")
 print("1. Gay Rating")
 print("2. 8ball")
 print("3. Flip a coin")
-print("4. Random insult (very work in progress)")
+print("4. Random insult")
 print("5. Ask Trump")
 
 #the lists
 ball8 = ['Yes', 'No', 'Hell naw', 'Doubtful...', 'Sure', 'Absolutely', 'You already know the answer to that...', 'I don`t know, m8', 'Yes, definitely! :D', 'YES YES YES!', 'Nope', 'Most likely!', 'In your dreams!', 'If that`s what you want...', 'Oh God, no.'] #ball8 since i don't think python likes numbers at the begginning of names
 asktrump = ['PRESIDENT', 'MELANIA', 'OIL', 'MIKE PENCE', 'BUSH', 'PEDOPHILE', 'HOMOSEXUAL', 'REAL', 'IRAN','THE BEST', 'AFGHANISTAN', 'MESS', 'FAKE MEDIA', 'PUTIN', 'ALIENS', 'RIGGED', 'DOING POORLY']
+insults = ['I don`t exactly hate you, but if you were on fire and I had water, I`d drink it.', ', I tried to see things from your perspective, but I couldn`t seem to shove my head that far up my ass.', ', I wasn`t born with enough middle fingers to let you know how I feel about you.', 'Hey, your village called – they want their idiot back.', 'It`s kinda sad watching you attempt to fit your entire vocabulary into a sentence.', 'You`re so fat when you go on an airplane, you have to pay baggage fees for your ass.', 'I`m not saying I hate you, but I would unplug your life support to charge my phone.', 'You`re so fat when you go swimming the whales start singing "We Are Family".', 'That little voice in the back of your head, telling you you`ll never be good enough? It`s right.', 'You bring everyone a lot of joy, when you leave the room.', 'If I ate a bowl of alphabet soup, I could shit out a smarter sentence than any of yours.', 'I would agree with you but then we would both be wrong.', 'You`re so fat that when you farted you started global warming.', 'What`s the difference between you and Hitler? Hitler knew when to kill himself.', 'Why is it acceptable for you to be an idiot but not for me to point it out?', 'I`m sorry I didn`t get that - I don`t speak idiot.']
 
 usrinput = input("? ")
 
@@ -43,7 +44,9 @@ if usrinput == "3":
         print("threw a coin: heads!")
 #Random insult
 if usrinput == "4": #remind me to finish this
-    num = str(random.randint(1,2))
+    insult = str(random.choice(insults))
+    name = input("Who do you want to insult? ")
+    print(name + ", " + insult)
 #Ask Trump
 if usrinput == "5":
     usrinput = input("What do you want to ask Trump? ")
